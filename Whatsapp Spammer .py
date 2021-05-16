1b76ccf63f9d3b69849b35e01c867fff68fe4f60
rickdgamer3 <67456004+rickdgamer3@users.noreply.github.com>
2021-05-16 21:33:31 +0500

    Add files via upload

---
+++ Whatsapp Spammer .py
@@ -0,0 +1,17 @@
+import pyautogui 
+import webbrowser as wb
+import time
+wb.open("web.whatsapp.com")
+time.sleep(10)
+for i in range(10):
+    pyautogui.press("Y")
+    pyautogui.press("S")
+    pyautogui.press("M")
+    pyautogui.press("S")
+    pyautogui.press("P")
+    pyautogui.press("A")
+    pyautogui.press("M")
+    pyautogui.press("S")
+    pyautogui.press("enter")
+    pyautogui.press("enter") 
+   
